9d05d1569ec70ac70bd27e6239910750bd81e5f4
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2021-10-11 05:28:36 +0100

    Tests sorted!

--- build/run-tests.ps1
+++ build/run-tests.ps1
@@ -1,36 +1,24 @@
-$resultsDir = Join-Path $env:APPVEYOR_BUILD_FOLDER TestResults
 $dotnet = Get-Command dotnet
 $testsPassed = $true
 
 try
 {
-    try
-    {
-        $wc = [System.Net.WebClient]::new()
+    $(
+        Get-ChildItem -Path (Join-Path $env:APPVEYOR_BUILD_FOLDER tests) -Filter *.Tests.Unit.csproj -Recurse
+        Get-ChildItem -Path (Join-Path $env:APPVEYOR_BUILD_FOLDER tests) -Filter *.Tests.Integration.csproj -Recurse
+    ) |
+    Foreach-Object {
 
-        $(
-            Get-ChildItem -Path (Join-Path $env:APPVEYOR_BUILD_FOLDER tests) -Filter *.Tests.Unit.csproj -Recurse
-            Get-ChildItem -Path (Join-Path $env:APPVEYOR_BUILD_FOLDER tests) -Filter *.Tests.Integration.csproj -Recurse
-        ) |
-        Foreach-Object {
+        Write-Host
+        Write-Host "##" $_.BaseName
 
-            Write-Host
-            Write-Host "##" $_.BaseName
-            $logFileName = "$($_.BaseName).trx"
+        & $dotnet test $_.FullName --test-adapter-path:. --logger:Appveyor
 
-            & $dotnet test $_.FullName --test-adapter-path:. --logger:Appveyor
-
-            if ($LASTEXITCODE -ne 0)
-            {
-                $testsPassed = $false
-            }
-
-            # $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", (Join-Path $resultsDir $logFileName))
+        if ($LASTEXITCODE -ne 0)
+        {
+            $testsPassed = $false
         }
-    }
-    finally
-    {
-        $wc.Dispose()
+
     }
 
     if (-not $testsPassed)
