74989a74c95c38b747068051a6937a3bfaa64359
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2021-12-07 06:50:02 +0000

    Enhance PropertyPath - Cloning - Convert to Newtonsoft.Json path

--- src/Firefly.CloudFormationParser/Utils/PropertyPath.cs
+++ src/Firefly.CloudFormationParser/Utils/PropertyPath.cs
@@ -11,11 +11,67 @@
     public class PropertyPath : Stack<string>
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyPath"/> class.
+        /// </summary>
+        public PropertyPath()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyPath"/> class.
+        /// </summary>
+        /// <param name="pathComponents">The path components.</param>
+        public PropertyPath(IEnumerable<string> pathComponents)
+        {
+            foreach (var c in pathComponents)
+            {
+                this.Push(c);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyPath"/> class.
+        /// </summary>
+        /// <param name="other">Another property path.</param>
+        private PropertyPath(PropertyPath other)
+        {
+            foreach (var prop in other.Reverse())
+            {
+                this.Push(prop);
+            }
+        }
+
+        /// <summary>
         /// Gets the current path.
         /// </summary>
         /// <value>
         /// The path.
         /// </value>
         public string Path => string.Join(".", this.Reverse());
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>A new <see cref="PropertyPath"/> with the current stack of elements</returns>
+        public PropertyPath Clone()
+        {
+            return new PropertyPath(this);
+        }
+
+        /// <summary>
+        /// Converts this path to a JSON path suitable for use with SelectToken
+        /// </summary>
+        /// <returns>JSON path</returns>
+        /// <seealso href="https://www.newtonsoft.com/json/help/html/SelectToken.htm">Querying JSON with SelectToken</seealso>
+        public string ToJsonPath()
+        {
+            if (!this.Any())
+            {
+                return string.Empty;
+            }
+
+            return this.Reverse().Select(p => p.All(char.IsDigit) ? $"[{p}]" : p)
+                .Aggregate((jp, next) => jp + (next.StartsWith("[") ? next : $".{next}"));
+        }
     }
 }
---
+++ tests/Firefly.CloudFormationParser.Tests.Unit/PropertyPathTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Firefly.CloudFormationParser.Tests.Unit
+{
+    using Firefly.CloudFormationParser.Utils;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class PropertyPathTests
+    {
+        [Theory]
+        [InlineData("Manufacturers.0.Name", "Manufacturers[0].Name")]
+        [InlineData("Manufacturers.0.Products.0.Price", "Manufacturers[0].Products[0].Price")]
+        [InlineData("Manufacturers.1.Products.0.Name", "Manufacturers[1].Products[0].Name")]
+        [InlineData("Foo.1.5.Bar", "Foo[1][5].Bar")]
+        public void ShouldConvertToJsonPath(string path, string expected)
+        {
+            var pp = new PropertyPath(path.Split('.'));
+
+            pp.ToJsonPath().Should().Be(expected);
+        }
+    }
+}
