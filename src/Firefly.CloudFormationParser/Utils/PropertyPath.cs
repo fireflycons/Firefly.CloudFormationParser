4047000a999fe828b0946a15da17be9813899c4b
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2021-12-14 20:37:28 +0000

    Implement IEquatable

--- src/Firefly.CloudFormationParser/Utils/PropertyPath.cs
+++ src/Firefly.CloudFormationParser/Utils/PropertyPath.cs
@@ -1,5 +1,6 @@
 ﻿namespace Firefly.CloudFormationParser.Utils
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
@@ -8,7 +9,7 @@
     /// Represents a path to a given property within an object graph such as an item within a template.
     /// </summary>
     [DebuggerDisplay("{Path}")]
-    public class PropertyPath : Stack<string>
+    public class PropertyPath : Stack<string>, IEquatable<PropertyPath>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="PropertyPath"/> class.
@@ -30,7 +31,12 @@
         }
 
         /// <summary>
+        /// <para>
         /// Initializes a new instance of the <see cref="PropertyPath"/> class.
+        /// </para>
+        /// <para>
+        /// This is effectively a clone mechanism so that a point in time capture of the current path can be taken.
+        /// </para>
         /// </summary>
         /// <param name="other">Another property path.</param>
         private PropertyPath(PropertyPath other)
@@ -42,6 +48,14 @@
         }
 
         /// <summary>
+        /// Gets the path depth (number of path components on the stack).
+        /// </summary>
+        /// <value>
+        /// The depth.
+        /// </value>
+        public int Depth => this.Count;
+
+        /// <summary>
         /// Gets the current path.
         /// </summary>
         /// <value>
@@ -50,12 +64,88 @@
         public string Path => string.Join(".", this.Reverse());
 
         /// <summary>
+        /// Implements the operator ==.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator ==(PropertyPath? left, PropertyPath? right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Implements the operator !=.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator !=(PropertyPath? left, PropertyPath? right)
+        {
+            return !Equals(left, right);
+        }
+
+        /// <summary>
         /// Clones this instance.
         /// </summary>
         /// <returns>A new <see cref="PropertyPath"/> with the current stack of elements</returns>
         public PropertyPath Clone()
         {
             return new PropertyPath(this);
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other">other</paramref> parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(PropertyPath? other)
+        {
+            return other != null && this.SequenceEqual(other);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>
+        /// true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((PropertyPath)obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
 
         /// <summary>
